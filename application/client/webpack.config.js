577fe202d6596d62c18e28894a735db2d901f6d9
snickerjp <snickerjp@gmail.com>
2026-03-20 22:05:43 +0900

    perf: remove unnecessary AudioContext ProvidePlugin

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -63,9 +63,6 @@ const config = {
     clean: true,
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      AudioContext: ["standardized-audio-context", "AudioContext"],
-    }),
     new webpack.EnvironmentPlugin({
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
